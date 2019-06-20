c85446971d1554c47a796986d91bb1c11e634d50
Abubakar Oladeji <abubakar@farmlogs.com>
2019-06-20 08:38:50 +0100

    User Model
    * Ensured user model implements the OK interface for verifying is a model is good to go or not

--- models/users.go
+++ models/users.go
@@ -1,5 +1,11 @@
 package models
 
+type missingFieldError string
+
+func (m missingFieldError) Error() string {
+	return string(m) + " is required"
+}
+
 // User :- Use for constructing the user's information
 type User struct {
 	ID        int    `json:"user_id"`
@@ -9,6 +15,28 @@ type User struct {
 	Password  string `json:"password"`
 	CreatedAt string `json:"createdAt"`
 	UpdatedAt string `json:"updatedAt"`
+}
+
+// OK represents types capable of validating themselves.
+// This would be utilized when decoding from request body.
+// Here, we can decide to check for required fields as needed.
+func (u *User) OK() error {
+	if len(u.Firstname) == 0 {
+		return missingFieldError("first_name")
+	}
+
+	if len(u.Lastname) == 0 {
+		return missingFieldError("last_name")
+	}
+
+	if len(u.Email) == 0 {
+		return missingFieldError("email")
+	}
+
+	if len(u.Password) == 0 {
+		return missingFieldError("password")
+	}
+	return nil
 }
 
 // UserStore :- This is a dictionary of users
