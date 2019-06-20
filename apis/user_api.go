e0e6a6116ad3ef3ce9d37d188571cae3128fa45e
Abubakar Oladeji <abubakar@farmlogs.com>
2019-06-20 08:32:14 +0100

    User API
    * Added decode function for decoding request body, and validating

--- apis/user_api.go
+++ apis/user_api.go
@@ -11,6 +11,12 @@ import (
 	"github.com/tonespy/simple-api/router"
 )
 
+// ok represents types capable of validating
+// themselves.
+type ok interface {
+	OK() error
+}
+
 // CreateUser :- Handler for creating a user
 // post /user
 func createUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
@@ -38,4 +44,14 @@ func GenerateUserRoutes() []router.Route {
 	routes := []router.Route{createUserRoute}
 
 	return routes
+}
+
+// decode can be this simple to start with, but can be extended
+// later to support different formats and behaviours without
+// changing the interface.
+func decode(r *http.Request, v ok) error {
+	if err := json.NewDecoder(r.Body).Decode(v); err != nil {
+		return err
+	}
+	return v.OK()
 }
