a490f8a3d85abc5d760ab4f61171f62bfbf2caf5
Abubakar Oladeji <abubakar@farmlogs.com>
2019-06-21 09:13:15 +0100

    User Model
    * Ensured all helper functions in user model is tested and verified

--- models/users_test.go
+++ models/users_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestUsers_UserStore(t *testing.T) {
+	UserStore = make(map[string]User)
 	// Check if user store is empty
 	assert.Empty(t, UserStore)
 
@@ -21,4 +22,52 @@ func TestUsers_UserStore(t *testing.T) {
 
 	// Verify user store count is greater than zero
 	assert.True(t, len(UserStore) > 0)
+}
+
+func TestUsers_GeneratingUserID(t *testing.T) {
+	UserStore = make(map[string]User)
+	assert.Empty(t, UserStore)
+
+	// Empty user should return 1
+	assert.Equal(t, 1, GenerateUserID())
+
+	// Add user to user store
+	UserStore = map[string]User{
+		"1": User{
+			ID:        1,
+			Firstname: "Abubakar",
+			Lastname:  "Oladeji",
+		},
+	}
+
+	assert.Equal(t, 2, GenerateUserID())
+}
+
+func TestUsers_OK(t *testing.T) {
+	UserStore = make(map[string]User)
+	assert.Empty(t, UserStore)
+	user := User{}
+
+	err := user.OK()
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "first_name")
+
+	user.Firstname = "Abubakar"
+	err = user.OK()
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "last_name")
+
+	user.Lastname = "Oladeji"
+	err = user.OK()
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "email")
+
+	user.Email = "abc@abc.com"
+	err = user.OK()
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "password")
+
+	user.Password = "password"
+	err = user.OK()
+	assert.Nil(t, err)
 }
